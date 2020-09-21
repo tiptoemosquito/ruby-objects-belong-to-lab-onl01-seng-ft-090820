1c43decb90715fdaff9a6ecefe22dfc7d274a5b2
Christina Howard <clhoward33@icloud.com>
2020-09-21 19:25:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,3 @@
-class Song
-
-  attr_accessor :title
-
-  def initialize(title)
-    @title = title
-  end
-
+def initialize(title)
+  @title = title
 end
-
-7/11 = Song.new("7/11")
-7/11.artist = beyonce
